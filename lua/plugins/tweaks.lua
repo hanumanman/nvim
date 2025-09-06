aa7c04ae44ac41cbfaf5feb72677d26087f52a5a
itztla <hanumanman37@gmail.com>
2025-09-06 16:27:12 +0700

    format: replace prettier with prettierd
    Lazyvim moved on from prettierd because some ppl was experiencing file
    truncation. I'll take that risk bc me want fast formatting

--- lua/plugins/tweaks.lua
+++ lua/plugins/tweaks.lua
@@ -1,6 +1,40 @@
 -- For overriding LazyVim's default plugins
 local bufferline = require("bufferline")
 return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      -- Override prettier with prettierd for supported file types
+      local supported_filetypes = {
+        "css",
+        "graphql",
+        "handlebars",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "less",
+        "markdown",
+        "markdown.mdx",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "yaml",
+      }
+
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      for _, ft in ipairs(supported_filetypes) do
+        opts.formatters_by_ft[ft] = { "prettierd" }
+      end
+      return opts
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "prettierd" } },
+  },
   -- Add border for blink UI
   {
     "saghen/blink.cmp",
