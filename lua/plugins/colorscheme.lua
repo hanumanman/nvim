d711ea1a2f7f2a0320f22bc72bb53788bacdda08
itztla <hanumanman37@gmail.com>
2026-03-22 01:21:45 +0700

    whatever

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,8 @@
-local function set_highlights(highlights, definition)
-	for _, group in ipairs(highlights) do
-		vim.api.nvim_set_hl(0, group, definition)
-		break
-	end
-end
-
 return {
 	{
 		"rebelot/kanagawa.nvim",
 		priority = 1000,
+		enabled = false,
 		lazy = false,
 		config = function()
 			require("kanagawa").setup({
@@ -59,39 +53,21 @@ return {
 		end,
 	},
 	{
-		"ellisonleao/gruvbox.nvim",
-		-- DISABLED
-		enabled = false,
+		"Mofiqul/vscode.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
 			vim.o.background = "dark"
-			vim.cmd([[colorscheme gruvbox]])
-
-			set_highlights({ "@lsp.type.variable" }, { bold = true })
-			set_highlights({
-				"BlinkCmpMenu",
-				"StatusLine",
-				"Pmenu",
-				"Float",
-				"NormalFloat",
-				"FloatBorder",
-				"LspReferenceText",
-				"BlinkCmpDoc",
-				"BlinkCmpDocBorder",
-			}, { bg = "none" })
-			set_highlights(
-				{ "LspSignatureActiveParameter", "FlashMatch" },
-				{ italic = true, fg = "#ff8349", underline = true, bold = true }
-			)
-			set_highlights({
-				"@lsp.type.method",
-				"@lsp.type.function",
-				"@lsp.typemod.function",
-				"@function.member",
-				"@function.call",
-			}, { italic = true })
-			set_highlights({ "@comment", "@lsp.type.comment" }, { italic = true, fg = "#6D7F8B" })
+			require("vscode").setup({
+				group_overrides = {
+					FlashBackdrop = { fg = "gray" }, -- gray
+					FlashMatch = { link = "SpecialKey" },
+					FlashLabel = { link = "CurSearch" },
+					["@function"] = { italic = true },
+					["@lsp.type.variable"] = { bold = true },
+				},
+			})
+			vim.cmd([[colorscheme vscode]])
 		end,
-		opts = {},
 	},
 }
